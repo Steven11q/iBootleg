7cf2fef0bdf7a2a69f8e7cb6c2153d8955ef1ac4
Steven11q <70647969+Steven11q@users.noreply.github.com>
2026-05-01 16:49:23 -0400

    Update iBootleg.sh

--- iBootleg.sh
+++ iBootleg.sh
@@ -17,7 +17,6 @@ if echo $album | grep -q NA; then
 
     #Changes album name to the name of the video, as there is no playlist title
     album=$(yt-dlp --skip-download --print title $1)
-    #echo $album
 
     #Download the Files
     yt-dlp -t mp3 -o "NA" --embed-thumbnail --split-chapters -o "chapter:%(section_number)s@%(section_title)S.%(ext)s" $1
@@ -25,70 +24,49 @@ if echo $album | grep -q NA; then
     #Establishes index, in case the video is a single
     index=0
 
-    #Prepare Metadata
+    #Iterate over playlist items
     IFS=$'\n'; for l in $(ls);
     do
 
-        #echo "l: $l"
-
+        #Set the index and title variable to the title and index of the video
         IFS=$'@' read index title <<<$(echo $l);
 
-
-
-        #echo "$title"
-
-        #echo "1 Index_title: $l"
+        #Set the metadata of the file so they are imported as an album
         mid3v2 "$l" -T "$index"
         mid3v2 "$l" -A "$album"
 
+        #Rename the file to remove the index
         mv $l $title
-
-
 
     done
 
+    #Remove the temporary NA file
     rm 'NA.mp3'
-
-
-
-    #my_array[1]="new_value"
 
 else
     #This command uses playlists
 
-    echo "album: $album"
-
-
     #Download The files
     yt-dlp -t mp3 --embed-thumbnail -o "%(playlist_index)s@%(title)S.%(ext)s" $1
 
-
-
-    #prepare Metadata
+    #Iterate over playlist items
     IFS=$'\n'; for l in $(ls);
     do
+        #Set the index and title variable to the title and index of the video
         IFS=$'@' read index title <<<$(echo $l);
 
-        #echo index: $index
-        #echo title: $title
-
-
-
-        echo "$l";
-
+        #Set the metadata of the file so they are imported as an album
         mid3v2 "$l" -T "$index"
         mid3v2 "$l" -A "$album"
-        mv $l $title
 
+        #Rename the file to remove the index
+        mv $l $title
 
     done
 
-
-
-
 fi
 
-
+#Move files to the iTunes library
 mv '/tmp/iBootleg/' '/home/$USER/Music/iTunes/iTunes Media/Automatically Add to iTunes/temp'
 
 
